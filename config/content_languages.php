b7ed80d449e715aa33c62ec2c5c3c8e9d0b174de
Taras <tarid1997@gmail.com>
2020-03-01 14:31:18 +0100

    import library for show code blocks

---
+++ config/content_languages.php
@@ -0,0 +1,17 @@
+<?php
+return [
+
+    //language data https://github.com/EnlighterJS/EnlighterJS
+
+    'code' => [
+        'PHP' => 'php',
+        'Javascript ' => 'js',
+        'Jquery' => 'jquery',
+        'Css' => 'css',
+        'Json' => 'json',
+    ],
+    'text' => [
+        'Polski' => 'pl',
+        'Angielski' => 'en',
+    ]
+];
